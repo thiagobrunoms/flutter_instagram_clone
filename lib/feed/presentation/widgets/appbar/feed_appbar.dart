14d645490fae01765c8cc3179e4ab315ee8921f4
Thiago de Sales <thiagobrunoms@gmail.com>
2022-07-09 15:24:31 -0300

    Some minor refactoring

--- lib/feed/presentation/widgets/appbar/feed_appbar.dart
+++ lib/feed/presentation/widgets/appbar/feed_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:instagram_clone/feed/presentation/widgets/appbar/instagram_logo_menu/instagram_logo_menu.dart';
 
 class FeedAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -14,22 +15,23 @@ class FeedAppBar extends StatelessWidget implements PreferredSizeWidget {
     return AppBar(
       backgroundColor: Theme.of(context).backgroundColor,
       leading: const InstagramLogoMenu(),
-      leadingWidth: size.width * 0.40,
+      leadingWidth: size.width * 0.35,
       actions: [
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.add_box_outlined),
-          iconSize: size.height * 0.035,
+          // icon: const Icon(Icons.add_box_outlined),
+          icon: const FaIcon(FontAwesomeIcons.squarePlus),
+          iconSize: size.height * 0.03,
         ),
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.favorite_border),
-          iconSize: size.height * 0.035,
+          icon: const Icon(FontAwesomeIcons.heart),
+          iconSize: size.height * 0.03,
         ),
         IconButton(
           onPressed: () {},
-          icon: const Icon(Icons.chat_bubble_outline),
-          iconSize: size.height * 0.035,
+          icon: const Icon(FontAwesomeIcons.comment),
+          iconSize: size.height * 0.03,
         ),
       ],
     );
--- lib/feed/presentation/widgets/post/post_widget.dart
+++ lib/feed/presentation/widgets/post/post_widget.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/feed/presentation/widgets/nicknames/active_style.dart';
-import 'package:instagram_clone/feed/presentation/widgets/post/description/post_description.dart';
-import 'package:instagram_clone/feed/presentation/widgets/stories/active_avatar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/actions/post_actions_widget.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/comments/post_comments_title_widget.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/content/post_content_widget.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/description/post_description.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/likes/post_likes_widget.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/post_timeout.dart';
+import 'package:instagram_clone/feed/presentation/widgets/post/widgets/top_post/top_post_widget.dart';
 
 class PostWidget extends StatefulWidget {
   const PostWidget({Key? key}) : super(key: key);
@@ -19,96 +23,15 @@ class _PostWidgetState extends State<PostWidget> {
     print('rebuilding feed');
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        _buildTopPost(),
-        _buildVerticalSpace(),
-        Image.asset('assets/post-test.png'),
-        _buildActions(),
-        _buildLikes(),
-        const PostDescriptionWidget(),
+      children: const [
+        TopPostWidget(),
+        PostContentWidget(),
+        PostActionsWidget(),
+        PostLikesWidget(),
+        PostDescriptionWidget(),
+        CommentsTitleWidget(),
+        PostTimeoutWidget()
       ],
-    );
-  }
-
-  Widget _buildTopPost() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          child: Row(
-            children: [
-              ActiveAvatar.small().create(),
-              _buildHorizontalSpace(),
-              ActiveStyleNickname('thiago.desales', 14).create(),
-            ],
-          ),
-        ),
-        SizedBox(
-            width: 40,
-            child: IconButton(
-              icon: const Icon(
-                Icons.more_vert,
-                color: Colors.white,
-              ),
-              onPressed: () {},
-            )),
-      ],
-    );
-  }
-
-  Widget _buildActions() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Expanded(
-          child: Row(
-            children: [
-              IconButton(
-                icon: _buildIcon(Icons.favorite_border),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: _buildIcon(Icons.mode_comment_outlined),
-                onPressed: () {},
-              ),
-              IconButton(
-                icon: _buildIcon(Icons.send),
-                onPressed: () {},
-              ),
-            ],
-          ),
-        ),
-        SizedBox(
-          width: 40,
-          child: IconButton(
-            icon: _buildIcon(
-              Icons.bookmark_border,
-            ),
-            onPressed: () {},
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget _buildIcon(IconData iconData) => Icon(
-        iconData,
-        color: Colors.white,
-        size: MediaQuery.of(context).size.height * 0.035,
-      );
-
-  Widget _buildHorizontalSpace() => SizedBox(
-        width: MediaQuery.of(context).size.width * 0.03,
-      );
-
-  Widget _buildVerticalSpace() => const SizedBox(
-        height: 10,
-      );
-
-  Widget _buildLikes() {
-    return const Text(
-      '2.150 curtidas',
-      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
     );
   }
 }
